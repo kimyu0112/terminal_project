e1a7854299be4f6997ceacc6d84c5dd03ba616d1
kimyu0112 <kimyu0112@outlook.com>
2024-05-02 23:55:11 +1000

    check_python bash scrip added 1

--- src/run.sh
+++ src/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-python3 -m venv .venv
+# python3 -m venv .venv
 source .venv/bin/activate
-pip3 install requests
+pip3 install --disable-pip-version-check --quiet -r requirements.txt
 python3 main.py
