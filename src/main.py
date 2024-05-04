981107d1256a3f8ae67c78f67631a6edcb971cdd
kimyu0112 <kimyu0112@outlook.com>
2024-05-04 22:53:01 +1000

    more comments added

--- src/main.py
+++ src/main.py
@@ -45,7 +45,7 @@ CURRENCY_LIST = {
 # Feature 1: allow users to access currency codes
 def access_currency_list():
     pprint(CURRENCY_LIST)
-
+    
 # Feature 2: real time currency converter
 def real_time_currency_conversion():
     while True:  # Loop to check whether user is putting in a valid currency code to convert from
@@ -76,9 +76,9 @@ def real_time_currency_conversion():
         response = requests.get(f"https://api.frankfurter.app/latest?amount={amount}&from={from_currency}&to={to_currency}")
         if response.status_code == 200:
             print(f"{amount}{from_currency} is {response.json()['rates'][to_currency]}{to_currency} on {TODAY}")
-        else:
+        else:  # Else/error handling when fetch is not successful
             print("Rates fetching from data source is not successful, please try our rates exchange converter later")
-    except:
+    except:  # Catch other errors not handled by else
         print("Rates fetching from data source is not successful, please try our rates exchange converter later")
 
 
@@ -89,12 +89,12 @@ def get_url(url):  # To return retrieve status code and return relevant informat
         content = response.text
         return status_code, content
     
-    except requests.exceptions.RequestException as e:
+    except requests.exceptions.RequestException as e:  # error handling when fetch is not successful, return nill outputs
         return None, None
 
-
+# Feature 3: historical currency converter
 def get_historical_rate():
-    while True:
+    while True:  # Loop to check whether user is putting in a valid currency code to convert from
         from_currency = str(input("Please enter in the currency code you'd like to convert from: ")).upper()
         
         if from_currency in CURRENCY_LIST:
@@ -104,7 +104,7 @@ def get_historical_rate():
             print("Please input correct currency from below list!")
             pprint(CURRENCY_LIST)
 
-    while True:      
+    while True:  # Loop to check whether user is putting in a valid currency code to convert to 
         to_currency = str(input("Please enter in the currency code you'd like to convert to: ")).upper()
         
         if to_currency in CURRENCY_LIST:
@@ -114,7 +114,7 @@ def get_historical_rate():
             print("Please input correct currency code from below list!")
             print(CURRENCY_LIST)
     
-    while True:   
+    while True:   # Loop to check whether user is putting in historical date in valid format
         try:
             date_str = input("Please enter date in YYYY-MM-DD format: ")
             date_format = "%Y-%m-%d"
@@ -125,47 +125,48 @@ def get_historical_rate():
         except:
             print("Please input date in YYYY-MM-DD format only!")
     
+    # If user puts in a future date get conversionrate for today/realtime
     if chosen_date < TODAY:
         chosen_date = chosen_date
     else:
         print("Please only put historical date, and since you put a future date we will use realtime currency converter")
         chosen_date = TODAY
     
-    try:
+    try:  #Historical currency converter
         status_code, content = get_url(f"{BASE_URL}/{chosen_date}?from={from_currency}&to={to_currency}&amount=100")
         if status_code == 200:
             data = json.loads(content)
             rate = data.get("rates", {}).get(to_currency)
-            print(f"100{from_currency} was {rate}{to_currency} on {chosen_date}")
-        else:
+            print(f"100{from_currency} was {rate}{to_currency} on {chosen_date}")  
+        else:  # Else/error handling when fetch is not successful
             print("Rates fetching from data source is not successful, please try our rates exchange converter later")
     
-    except:
+    except:  # Catch other errors not handled by else
         print("Rates fetching from data source is not successful, please try our rates exchange converter later")
 
-
+# User options interface
 def main():
     while True:
         try:
             user_option = int(input("Welcome to the currency converter!\n\n1. List of Currency code by Currency names\n2. Real Time Convert Converter\n3. Historical Exchange Rate\n4. Exit the function\n\nPlease select an option: "))
         
-            if user_option == 1:
-                access_currency_list()
-                nested_user_option = bool(input("Type anything if you want to proceed to currency conversion, or hit Enter if you want to exit the program: "))
-                
+            if user_option == 1:  
+                access_currency_list()  #Provide list of currency codes for users
+                nested_user_option = bool(input("Type anything if you want to proceed to currency conversion, or hit Enter if you want to exit the program: "))  #Ask user if they want to access realtime currency converter
+                #  If yess #Call realtime currency converter
                 if nested_user_option == True:
-                    real_time_currency_conversion()
+                    real_time_currency_conversion() 
                 break
     
-            elif user_option == 2:
+            elif user_option == 2:  #Call realtime currency converter
                 real_time_currency_conversion()
                 break
     
-            elif user_option == 3:
+            elif user_option == 3:  #Call histlrical currency converter
                 get_historical_rate()
                 break
         
-            elif user_option == 4:
+            elif user_option == 4:  #Exit the app
                 break
         
             else:
